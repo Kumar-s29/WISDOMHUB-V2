c394c78714186435f82f28271f80781027986fbe
Kumar-s29 <swamykumar29603@gmail.com>
2025-01-21 22:14:14 +0530

    Home page completed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Header from "../components/Header";
+import SpecialityMenu from "../components/SpecialityMenu";
+import TopMentors from "../components/TopMentors";
+import Banner from "../components/Banner";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <SpecialityMenu />
+      <TopMentors />
+      <Banner />
     </div>
   );
 };
