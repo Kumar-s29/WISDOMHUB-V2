4418eec13dd0d2546936e5ed8e7b625f45b27414
Kumar-s29 <swamykumar29603@gmail.com>
2025-02-04 21:39:10 +0530

    chages added

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -13,6 +13,8 @@ const AppContextProvider = ({ children }) => {
     localStorage.getItem("token") ? localStorage.getItem("token") : false
   );
 
+  const [userData, setUserData] = useState(false);
+
   // Fetch mentors data
   const getMentorsData = async () => {
     try {
@@ -28,9 +30,23 @@ const AppContextProvider = ({ children }) => {
     }
   };
 
-  useEffect(() => {
-    getMentorsData();
-  }, []);
+  const loadUserProfileData = async () => {
+    try {
+      const { data } = await axios.get(`${backendUrl}/api/user/get-profile`, {
+        headers: {
+          token,
+        },
+      });
+      if (data.success) {
+        setUserData(data.userData);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
 
   const value = {
     mentors,
@@ -39,9 +55,21 @@ const AppContextProvider = ({ children }) => {
     setToken,
     backendUrl,
     doctors,
+    userData,
+    setUserData,
+    loadUserProfileData,
     // Provide mentors in the context
   };
-
+  useEffect(() => {
+    getMentorsData();
+  }, []);
+  useEffect(() => {
+    if (token) {
+      loadUserProfileData();
+    } else {
+      setUserData(false);
+    }
+  }, [token]);
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
